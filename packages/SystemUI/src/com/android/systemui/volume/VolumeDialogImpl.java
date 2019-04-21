43cad41795d6cc89ec4ec92a0f5437a467a613b6
Shreyansh Lodha <slodha96@gmail.com>
2019-04-21 06:18:15 +0530

    ExpandingVolPanel: Show only Music Stream when collapsed.
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>
    Change-Id: Ifd09bd27e40fd031d2c69e489c762137fd82a40b
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -428,7 +428,7 @@ public class VolumeDialogImpl implements VolumeDialog {
         for(int i = mRows.size() - 1; i >= 0; i--) {
             final VolumeRow row = mRows.get(i);
             if ((row.stream == AudioManager.STREAM_RING ||
-                    row.stream == AudioManager.STREAM_ALARM) && row.stream != mActiveStream) {
+                    row.stream == AudioManager.STREAM_ALARM)) {
                 Util.setVisOrGone(row.view, /* vis */ false);
             }
         }
@@ -482,6 +482,9 @@ public class VolumeDialogImpl implements VolumeDialog {
                 }
                 mExpanded = true;
             } else {
+                VolumeRow row = findRow(AudioManager.STREAM_MUSIC);
+                updateVolumeRowTintH(row,
+                            /* isActive */ true);
                 cleanExpandRows();
                 mExpanded = false;
             }
