adinkwok/android_frameworks_base_forked

CaffeineTile: Default to infinity w/o titles
If QS titles are disabled, default to infinity since it is redundant;
user cannot see the countdown anyways.

Change-Id: Ib0258842b866a34fee371608fd43853b35bb1b4f

packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
@@ -25,6 +25,7 @@ import android.net.Uri;
 import android.os.CountDownTimer;
 import android.os.PowerManager;
 import android.os.SystemClock;
+import android.os.UserHandle;
 import android.provider.Settings;
 
 import android.service.quicksettings.Tile;
@@ -103,10 +104,17 @@ public class CaffeineTile extends QSTileImpl<BooleanState> {
                 }
             }
         } else {
+            boolean showTitles = Settings.System.getIntForUser(
+                    mContext.getContentResolver(), Settings.System.QS_TILE_HIDE_TITLE, 1,
+                    UserHandle.USER_CURRENT) == 1;
             // toggle
             if (mWakeLock.isHeld()) {
                 mWakeLock.release();
                 stopCountDown();
+            } else if (!showTitles) { // if QS titles are disabled, default to infinity
+                mWakeLock.acquire();
+                mDuration = DURATIONS.length - 1;
+                startCountDown(DURATIONS[mDuration]);
             } else {
                 mWakeLock.acquire();
                 mDuration = 0;

shagbag913/frameworks_base-1

Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
@@ -26,6 +26,7 @@ import android.net.Uri;
 import android.os.CountDownTimer;
 import android.os.PowerManager;
 import android.os.SystemClock;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.service.quicksettings.Tile;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
@@ -112,10 +113,17 @@ public class CaffeineTile extends QSTileImpl<BooleanState> {
                 }
             }
         } else {
+            boolean showTitles = Settings.System.getIntForUser(
+                    mContext.getContentResolver(), Settings.System.QS_TILE_TITLE_VISIBILITY, 1,
+                    UserHandle.USER_CURRENT) == 1;
             // toggle
             if (mWakeLock.isHeld()) {
                 mWakeLock.release();
                 stopCountDown();
+            } else if (!showTitles) { // if QS titles are disabled, default to infinity
+                mWakeLock.acquire();
+                mDuration = DURATIONS.length - 1;
+                startCountDown(DURATIONS[mDuration]);
             } else {
                 mWakeLock.acquire();
                 mDuration = 0;